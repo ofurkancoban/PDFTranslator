b6fc33b3379577607cda48a361b0feb783ce3207
ofurkancoban <f.coban93@gmail.com>
2025-05-15 13:54:54 +0200

    Deploy 20

--- src/utils/apiBase.ts
+++
@@ -1,6 +0,0 @@
-// src/utils/apiBase.ts
-export function getApiBase() {
-  // Eğer URL path’inde /pdftranslator… ile başlayan bir şey varsa onu yakala (Railway prod için)
-  const base = window.location.pathname.match(/^\/pdftranslator[^\/]*\//);
-  return base ? base[0] : '/';
-}
